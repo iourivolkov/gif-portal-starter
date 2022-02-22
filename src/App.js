38b419e1688ecb2e5c043c1fba2a425c35de2d6e
Iouri Volkov <volk.iouri@gmail.com>
2022-02-21 23:32:03 -0500

    display connect button if user is not signed in, add logic to connect to user's Phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,75 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  // state
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  // function to decide if Phantom wallet is connected
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+
+          // solana obj gives us function that will allow us to connect direclty w/ user's wallet
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connected with Public Key:",
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  // render this UI if the user hasn't connected
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect Wallet
+    </button>
+  );
+
+  // when component first mounts / loads -> check if user has Phantom wallet
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
   return (
     <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">Workr</p>
-          <p className="sub-text">
-            A decentralized job board powered by Solana
-          </p>
+      <div className={walletAddress ? "authed-container" : "container"}>
+        <div className="container">
+          <div className="header-container">
+            <p className="header">Workr</p>
+            <p className="sub-text">
+              A decentralized job board powered by Solana
+            </p>
+            {!walletAddress && renderNotConnectedContainer()}
+          </div>
         </div>
       </div>
     </div>
