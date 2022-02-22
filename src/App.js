7e59b40d404b993bb7ac090a8046f9ca6bbf0976
Iouri Volkov <volk.iouri@gmail.com>
2022-02-21 22:08:37 -0500

    install depdencies, change basic html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Workr</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            A decentralized job board powered by Solana
           </p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
